32a1d310bd16fbcd5c055843433ec655f2879616
ru51a4 <ru51a4@mail.ru>
2022-03-27 18:43:08 +0500

    reply

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -240,7 +240,7 @@
                 cReply = getCountReply();
             }
             div.innerHTML = document.querySelector(`.btn-reply[id="${id}"]`).parentElement.parentElement.parentElement.innerHTML;
-            el.target.parentElement.insertAdjacentElement("afterbegin", div);
+            el.target.insertAdjacentElement("afterbegin", div);
             if (cReply !== getCountReply()) {
                 cReply = getCountReply();
                 setEventsReply();
